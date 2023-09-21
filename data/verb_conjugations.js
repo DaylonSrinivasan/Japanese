b3dbe696e0df86de6dedb47cdbf7b778555139d0
DaylonSrinivasan <daylon.srinivasan@gmail.com>
2023-09-21 17:38:24 +0000

    Add verb conjugation quiz

---
+++ data/verb_conjugations.js
@@ -0,0 +1,122 @@
+export const VERB_CONJUGATIONS = [{
+  "dictionary": "たべる",
+  "negative": "たべない",
+  "polite": "たべます",
+  "polite_negative": "たべません",
+  "past": "たべた",
+  "past_negative": "たべなかった",
+  "past_polite": "たべました",
+  "past_polite_negative": "たべませんでした",
+  "te": "たべて"
+},
+{
+  "dictionary": "のむ",
+  "negative": "のまない",
+  "polite": "のみます",
+  "polite_negative": "のみません",
+  "past": "のんだ",
+  "past_negative": "のまなかった",
+  "past_polite": "のみました",
+  "past_polite_negative": "のみませんでした",
+  "te": "のんで"
+},
+{
+  "dictionary": "ある",
+  "negative": "ない",
+  "polite": "あります",
+  "polite_negative": "ありません",
+  "past": "あった",
+  "past_negative": "なかった",
+  "past_polite": "ありました",
+  "past_polite_negative": "ありませんでした",
+  "te": "あって"
+},
+{
+  "dictionary": "くる",
+  "negative": "こない",
+  "polite": "きます",
+  "polite_negative": "きません",
+  "past": "きた",
+  "past_negative": "こなかった",
+  "past_polite": "きました",
+  "past_polite_negative": "きませんでした",
+  "te": "きて"
+},
+{
+  "dictionary": "いく",
+  "negative": "いかない",
+  "polite": "いきます",
+  "polite_negative": "いきません",
+  "past": "いった",
+  "past_negative": "いかなかった",
+  "past_polite": "いきました",
+  "past_polite_negative": "いきませんでした",
+  "te": "いって"
+},
+{
+  "dictionary": "のぼる",
+  "negative": "のぼらない",
+  "polite": "のぼります",
+  "polite_negative": "のぼりません",
+  "past": "のぼった",
+  "past_negative": "のぼらなかった",
+  "past_polite": "のぼりました",
+  "past_polite_negative": "のぼりませんでした",
+  "te": "のぼって"
+},
+{
+  "dictionary": "ねる",
+  "negative": "ねらない",
+  "polite": "ねります",
+  "polite_negative": "ねりません",
+  "past": "ねた",
+  "past_negative": "ねらなかった",
+  "past_polite": "ねりました",
+  "past_polite_negative": "ねりませんでした",
+  "te": "ねて"
+},
+{
+  "dictionary": "よむ",
+  "negative": "よまない",
+  "polite": "よみます",
+  "polite_negative": "よみません",
+  "past": "よんだ",
+  "past_negative": "よまなかった",
+  "past_polite": "よみました",
+  "past_polite_negative": "よみませんでした",
+  "te": "よんで"
+},
+{
+  "dictionary": "かく",
+  "negative": "かかない",
+  "polite": "かきます",
+  "polite_negative": "かきません",
+  "past": "かいた",
+  "past_negative": "かかなかった",
+  "past_polite": "かきました",
+  "past_polite_negative": "かきませんでした",
+  "te": "かいて"
+},
+{
+  "dictionary": "はしる",
+  "negative": "はしらない",
+  "polite": "はしります",
+  "polite_negative": "はしりません",
+  "past": "はしった",
+  "past_negative": "はしらなかった",
+  "past_polite": "はしりました",
+  "past_polite_negative": "はしりませんでした",
+  "te": "はしって"
+},
+{
+  "dictionary": "およぐ",
+  "negative": "およがない",
+  "polite": "およぎます",
+  "polite_negative": "およぎません",
+  "past": "およいだ",
+  "past_negative": "およがなかった",
+  "past_polite": "およぎました",
+  "past_polite_negative": "およぎませんでした",
+  "te": "およいで"
+}
+]
---
+++ pages/verb_conjugation.js
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react';
+import { VERB_CONJUGATIONS } from '../data/verb_conjugations.js';
+
+function App() {
+  const [quizData, setQuizData] = useState(null);
+  const [userAnswer, setUserAnswer] = useState('');
+  const [feedback, setFeedback] = useState('');
+  const [enterPressCount, setEnterPressCount] = useState(0);
+
+  useEffect(() => {
+    setQuizData(getRandomQuizData());
+    setUserAnswer('');
+    setFeedback('');
+    setEnterPressCount(0);
+  }, []);
+
+  const getRandomQuizData = () => {
+    // Select a random verb and a random conjugation to quiz the user.
+    const randomVerbIndex = Math.floor(Math.random() * VERB_CONJUGATIONS.length);
+    const verb = VERB_CONJUGATIONS[randomVerbIndex];
+
+    const conjugations = Object.keys(verb).filter(
+      (key) => key !== 'dictionary'
+    );
+
+    const randomConjugationIndex = Math.floor(
+      Math.random() * conjugations.length
+    );
+    const conjugationToQuiz = conjugations[randomConjugationIndex];
+
+    return {
+      verb: verb.dictionary,
+      conjugationToQuiz,
+      correctAnswer: verb[conjugationToQuiz],
+    };
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.key === 'Enter') {
+      setEnterPressCount(enterPressCount + 1);
+
+      if (enterPressCount % 2 === 0) {
+        // Even number of Enter presses, show feedback
+        if (userAnswer === quizData.correctAnswer) {
+          setFeedback('Correct! Press Enter for a new quiz.');
+        } else {
+          setFeedback(`Incorrect. The correct answer is: ${quizData.correctAnswer}. Press Enter for a new quiz.`);
+        }
+      } else {
+        // Odd number of Enter presses, start a new quiz
+        startNewQuiz();
+      }
+    }
+  };
+
+  const startNewQuiz = () => {
+    setQuizData(getRandomQuizData());
+    setUserAnswer('');
+    setFeedback('');
+    setEnterPressCount(0);
+  };
+
+  return (
+    <div>
+      <h1>Verb Conjugation Quiz</h1>
+
+      {/* Display the quiz */}
+      {quizData && (
+        <div>
+          <h2>Quiz:</h2>
+          <p>Verb: {quizData.verb}</p>
+          <p>Conjugation to Quiz: {quizData.conjugationToQuiz}</p>
+          <label>Your Answer: </label>
+          <input
+            type="text"
+            value={userAnswer}
+            onChange={(e) => setUserAnswer(e.target.value)}
+            onKeyUp={handleKeyUp}
+          />
+          <p>{feedback}</p>
+        </div>
+      )}
+
+      {/* Display the table */}
+      <h2>Verb Conjugations Table</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Verb</th>
+            <th>Dictionary</th>
+            <th>Negative</th>
+            <th>Polite</th>
+            <th>Polite Negative</th>
+            <th>Past</th>
+            <th>Past Negative</th>
+            <th>Past Polite</th>
+            <th>Past Polite Negative</th>
+            <th>Te-form</th>
+          </tr>
+        </thead>
+        <tbody>
+          {VERB_CONJUGATIONS.map((verb, index) => (
+            <tr key={index}>
+              <td>{verb.dictionary}</td>
+              <td>{verb.dictionary}</td>
+              <td>{verb.negative}</td>
+              <td>{verb.polite}</td>
+              <td>{verb.polite_negative}</td>
+              <td>{verb.past}</td>
+              <td>{verb.past_negative}</td>
+              <td>{verb.past_polite}</td>
+              <td>{verb.past_polite_negative}</td>
+              <td>{verb.te}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
